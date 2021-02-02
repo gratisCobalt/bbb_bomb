3ba3e6fbb81e44e4c81833cf40834e80f14f7121
gratisCobalt <jaywatson@gmx.net>
2021-02-02 19:29:33 +0100

    just another commit

--- bomb.py
+++ bomb.py
@@ -21,7 +21,27 @@ from selenium.webdriver.common.keys import Keys
 
 import logo
 
+
 class bomb():
+
+    random_string = ''
+
+    # vars from json
+    audio_path = ''
+    site_url = ''
+    audio_device_name = ''
+    play_audio_from_file = False
+    write_text_to_chat = False
+    volume = 0
+    usernames = []
+
+    # check if step is alredy done
+    join_name = True
+    join = True
+    audio = True
+    audio_thumb = False
+    muted = True
+
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['--ignore-certificate-errors=yes', '--web-security=no', '--ssl-protocol=any'])
@@ -31,99 +51,105 @@ class bomb():
         self.driver = webdriver.Chrome(options=options)
 
     def run(self):
+        b.read_json()
+
+        self.driver.get(b.site_url)
+
+        b.random_string = b.get_random_string()
+        b.init_musicplayer()
+        
+        while (True):
+            b.join_room()
+            b.accept_audio()
+            b.unmute()
+            b.chat()
+
+    def read_json(self):
         with open('./Python/bomb/config.json') as f:
             data = f.read()
         obj = json.loads(data)
-        audio_path = str(obj['audio_path'])
-        site_url = str(obj['site_url'])
-        audio_device_name = str(obj['audio_device_name'])
-        play_audio_from_file = (obj['play_audio_from_file'])
-        write_text_to_chat = (obj['write_text_to_chat'])
-        volume = 1 # float(obj['volume'])
-        usernames = list(obj['usernames'])
 
-        self.driver.get(site_url)
+        b.audio_path = str(obj['audio_path'])
+        b.site_url = str(obj['site_url'])
+        b.audio_device_name = str(obj['audio_device_name'])
+        b.play_audio_from_file = (obj['play_audio_from_file'])
+        b.write_text_to_chat = (obj['write_text_to_chat'])
+        b.volume = 1 # float(obj['volume'])
+        b.usernames = list(obj['usernames'])
 
+    def get_random_string(self):
+        for _ in range(50):
+            random_integer = random.randint(97, 97 + 26 - 1)
+            flip_bit = random.randint(0, 1)
+            random_integer = random_integer - 32 if flip_bit == 1 else random_integer
+            b.random_string += (chr(random_integer))
+
+    def join_room(self):
+        while (b.join_name):
+            try:
+                join_input = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/form/div/input[4]')  # name input
+                join_input.clear()
+                join_input.send_keys(random.choice(b.usernames))
+                b.join_name = False
+                break
+            except Exception:
+                pass
+        
+        while (b.join):
+            try:
+                self.driver.find_element_by_xpath('//*[@id="room-join"]').click()  # join room button
+                b.join = False
+                break
+            except Exception:
+                pass
+
+    def init_musicplayer(self):
         mixer.init()  # music player
         [get_audio_device_name(x, 0).decode() for x in range(get_num_audio_devices(0))]
-        mixer.init(devicename=audio_device_name)
-        mixer.music.load(audio_path)
-        mixer.music.set_volume(volume)
+        mixer.init(devicename=b.audio_device_name)
+        mixer.music.load(b.audio_path)
+        mixer.music.set_volume(b.volume)
+        if (b.play_audio_from_file):
+            mixer.music.play(loops=-1)  # infinite loop
 
-        # check if step is alredy done
-        join_name = True
-        join = True
-        audio = True
-        audio_thumb = False
-        join_with_audio = True
-        muted = True
+    def accept_audio(self):
+        while (b.audio):
+            try:
+                self.driver.find_element_by_css_selector("body > div.portal--27FHYi > div > div > div.content--IVOUy > div > div > span > button:nth-child(1) > span.label--Z12LMR3").click()  # join with mic
+                b.audio_thumb = True
+            except Exception:
+                pass
 
-        random_string = ''
-        
-        if (play_audio_from_file):
-                mixer.music.play(loops=-1)  # infinite loop
-
-        while (True):
-            for _ in range(50):
-                random_integer = random.randint(97, 97 + 26 - 1)
-                flip_bit = random.randint(0, 1)
-                random_integer = random_integer - 32 if flip_bit == 1 else random_integer
-                random_string += (chr(random_integer))
-
-            while (join_name):
+            while (b.audio_thumb):
                 try:
-                    join_input = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/form/div/input[4]')  # name input
-                    join_input.clear()
-                    join_input.send_keys(random.choice(usernames))
-                    join_name = False
+                    self.driver.find_element_by_css_selector('body > div.portal--27FHYi > div > div > div.content--IVOUy > div > span > button:nth-child(1) > span.label--Z12LMR3').click()  # thumbs up audiotest
+                    b.audio_thumb = False
+                    b.audio = False
                     break
                 except Exception:
                     pass
 
-            while (join):
-                try:
-                    self.driver.find_element_by_xpath('//*[@id="room-join"]').click()  # join room button
-                    join = False
+    def unmute(self):
+        while (b.muted):
+            try:
+                mute_btn = self.driver.find_element_by_xpath('/html/body/div/main/section/div[1]/section[2]/div/div[2]/span/button[1]/span[1]/i') # mute button
+                if ('unmute' in mute_btn.get_attribute('class')):
+                    b.muted = False
                     break
-                except Exception:
-                    pass
+                else:
+                    mute_btn.click()
+                    b.muted = False
+                    break
+            except Exception:
+                pass
 
-            while (audio):
-                if (play_audio_from_file and join_with_audio):
-                    try:
-                        self.driver.find_element_by_css_selector("body > div.portal--27FHYi > div > div > div.content--IVOUy > div > div > span > button:nth-child(1) > span.label--Z12LMR3").click()  # join with mic
-                        join_with_audio = False
-                        audio_thumb = True
-                    except Exception:
-                        pass
-                    while (audio_thumb):
-                        try:
-                            self.driver.find_element_by_css_selector('body > div.portal--27FHYi > div > div > div.content--IVOUy > div > span > button:nth-child(1) > span.label--Z12LMR3').click()  # thumbs up audiotest
-                            audio_thumb = False
-                            audio = False
-                            break
-                        except Exception:
-                            pass
-
-            while (muted):
-                try:
-                    mute_btn = self.driver.find_element_by_xpath('/html/body/div/main/section/div[1]/section[2]/div/div[2]/span/button[1]/span[1]/i') # mute button
-                    if ('unmute' in mute_btn.get_attribute('class')):
-                        muted = False
-                        break
-                    else:
-                        mute_btn.click()
-                        muted = False
-                        break
-                except Exception:
-                    pass
-
-            if (write_text_to_chat):
-                text_send = self.driver.find_element_by_xpath('//*[@id="message-input"]')  # chat input
-                text_send.clear()
-                text_send.send_keys(random_string)
-
-                self.driver.find_element_by_xpath('/html/body/div/main/section/div[4]/section/div/form/div[1]/button/span[1]/i').click()  # chat send button
+    def chat(self):
+        if (b.write_text_to_chat):
+            text_send = self.driver.find_element_by_xpath('//*[@id="message-input"]')  # chat input
+            text_send.clear()
+            text_send.send_keys(random_string)
+            self.driver.find_element_by_xpath('/html/body/div/main/section/div[4]/section/div/form/div[1]/button/span[1]/i').click()  # chat send button
 
 logo.print_logo()
-bomb().run()
+b = bomb()
+b.run()
